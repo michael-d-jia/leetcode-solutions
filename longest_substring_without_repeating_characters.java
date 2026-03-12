f4098ced8a85d3f583b4e1c83cf813890445d740
Michael Jia <michael.d.jia@gmail.com>
2026-03-12 08:53:00 -0400

    finished longest substring

--- longest_substring_without_repeating_characters.java
+++ longest_substring_without_repeating_characters.java
@@ -43,7 +43,25 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         // TODO: Implement the solution here.
         // Placeholder return statement
-        return 0;
+        // sliding window problem
+        // implement left and right pointers
+        // max length variable set to 0
+        // loop using right pointer
+        // 
+        // check if difference between right pointer and left pointer is greater than max length
+        // if so replace current max length to the new one
+        int maxLength = 0;
+        int left = 0;
+        HashSet<Character> set = new HashSet<>();
+        for (int right = 0; right < s.length(); right++) {
+            while(set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, set.size());
+        }
+        return maxLength;
     }
 
     public static void main(String[] args) {

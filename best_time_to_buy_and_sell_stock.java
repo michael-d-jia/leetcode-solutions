e91d28503dba780fadab423bf8529e9698601943
Michael Jia <michael.d.jia@gmail.com>
2026-05-07 13:34:35 -0400

    finished best time to buy/sell stock

--- best_time_to_buy_and_sell_stock.java
+++ best_time_to_buy_and_sell_stock.java
@@ -36,9 +36,18 @@ class Solution {
      * 0 <= prices[i] <= 104
      */
     public int maxProfit(int[] prices) {
-        // Your solution logic will go here.
-        // For now, it returns a placeholder value.
-        return 0;
+        int profit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > profit) {
+                profit = prices[i] - minPrice;
+            }
+        }
+
+        return profit;
     }
 
     public static void main(String[] args) {

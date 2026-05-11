7a947704fabc985fcd75dd3aed57e326faddf6e6
Michael Jia <michael.d.jia@gmail.com>
2026-05-11 12:55:51 -0400

    finished top k frequency

--- top_k_frequent_elements.java
+++ top_k_frequent_elements.java
@@ -1,10 +1,6 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.ArrayList;
-import java.util.Collections; // For sorting lists if needed, but Arrays.sort is for int[]
 
 class Solution {
     /**
@@ -52,8 +48,25 @@ class Solution {
      * Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
      */
     public int[] topKFrequent(int[] nums, int k) {
-        // Placeholder return statement
-        return new int[0];
+        HashMap<Integer, Integer> mappedFrequencies = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            mappedFrequencies.put(nums[i], mappedFrequencies.getOrDefault(nums[i], 0) + 1);
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> mappedFrequencies.get(a) - mappedFrequencies.get(b));
+        
+        for (int key : mappedFrequencies.keySet()) {
+            minHeap.add(key);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = minHeap.poll();
+        }
+        return result;
     }
 
     public static void main(String[] args) {

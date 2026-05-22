e28dbec3dadfd545a62d9a049c6b7a73d71818c5
Michael Jia <michael.d.jia@gmail.com>
2026-05-22 13:44:19 -0400

    finished house robber

--- climbing_stairs.java
+++ climbing_stairs.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 /**
  * Problem: Climbing Stairs
  * Topic: DP
--- house_robber.java
+++ house_robber.java
@@ -35,8 +35,17 @@ class Solution {
      * 0 <= nums[i] <= 400
      */
     public int rob(int[] nums) {
-        // Placeholder return statement
-        return 0;
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int prev1 = Math.max(nums[1], nums[0]);
+        int prev2 = nums[0];
+        for (int i = 2; i < nums.length; i++) {
+            int current = Math.max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
     }
 
     public static void main(String[] args) {
